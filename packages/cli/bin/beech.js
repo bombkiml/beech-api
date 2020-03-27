0f19bde6da71632589ce570ff00e7d5b26648192
bombkiml <nattapat.jquery@gmail.com>
2020-03-27 22:59:35 +0700

    make structure for v3

---
+++ packages/cli/bin/beech.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+
+const chalk = require('chalk')
+const semver = require('semver')
+const requiredVersion = require('../package.json').engines.node
+const didYouMean = require('didyoumean')
+
+// Setting edit distance to 60% of the input string's length
+didYouMean.threshold = 0.6
+
+function checkNodeVersion (wanted, id) {
+  if (!semver.satisfies(process.version, wanted)) {
+    console.log(chalk.red(
+      'You are using Node ' + process.version + ', but this version of ' + id +
+      ' requires Node ' + wanted + '.\nPlease upgrade your Node version.'
+    ))
+    process.exit(1)
+  }
+}
+
+checkNodeVersion(requiredVersion, '@vue/cli')
+
+if (semver.satisfies(process.version, '9.x')) {
+  console.log(chalk.red(
+    `You are using Node ${process.version}.\n` +
+    `Node.js 9.x has already reached end-of-life and will not be supported in future major releases.\n` +
+    `It's strongly recommended to use an active LTS version instead.`
+  ))
+}
