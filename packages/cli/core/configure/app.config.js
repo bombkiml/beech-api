29d4f365981032f2cc77371b0c5cc8ed87a63d5c
nattapat chin <nattapat.reh@gmail.com>
2020-04-14 10:56:35 +0700

    fix mysql connect for resolve, update package.json, fix minor bug

--- packages/cli/core/configure/app.config.js
+++ packages/cli/core/configure/app.config.js
@@ -9,9 +9,9 @@
  */
 exports.main_config = {
   app_port: 9000,
-  app_host: '127.0.0.1',
-  client_host: 'https://example.com',
-  app_secret: ['2cc118cd91b52ff99e3c005ddced76fb']
+  app_host: "localhost",
+  client_host: "https://example.com",
+  app_secret: ["2cc118cd91b52ff99e3c005ddced76fb"]
 }
 
 /**
@@ -29,23 +29,23 @@ exports.main_config = {
  */
 exports.mySqlConfig = [
   {
-    name: 'default_db',
-    host: '127.0.0.1',
-    username: 'root',
-    password: '',
-    database: 'example1_db',
-    port: '3306',
-    charset: 'utf8',
+    name: "default_db",
+    host: "127.0.0.1",
+    username: "root",
+    password: "",
+    database: "example1_db",
+    port: "3306",
+    charset: "utf8",
     isConnect: false
   },
   {
-    name: 'second_db',
-    host: '127.0.0.1',
-    username: 'root',
-    password: '',
-    database: 'example2_db',
-    port: '3306',
-    charset: 'utf8',
+    name: "second_db",
+    host: "127.0.0.1",
+    username: "root",
+    password: "",
+    database: "example2_db",
+    port: "3306",
+    charset: "utf8",
     isConnect: false
   }
 ]
--- packages/cli/core/databases/mysql.connection.js
+++ packages/cli/core/databases/mysql.connection.js
@@ -3,7 +3,7 @@ exports.mySqlConnection = () => {
     try {
       global.mysql = {};
       // loop database connection
-      _config.mySqlConfig.map(val => {
+      _config.mySqlConfig.map((val, index) => {
         if (val.isConnect) {
           let connection = _mysql.createConnection({
             host: val.host,
@@ -17,12 +17,20 @@ exports.mySqlConnection = () => {
             if (!err) {
               mysql[val.name] = connection;
               console.log('[102m[90m Passed [0m [0mDatabase is connected at [[93m' + val.name, '[0m->[93m', connection.config.database + ':' + connection.config.port + '[0m]');
-              resolve(connection);
+              // checking for resolve
+              if ((index + 1) == _config.mySqlConfig.length) {
+                resolve(true);
+              }
             } else {
               console.log('[101m Failed [0m [91mDatabase `' + val.name + '` is connect failed.[0m');
               throw err;
             }
           })
+        } else {
+          // checking for resolve
+          if ((index + 1) == _config.mySqlConfig.length) {
+            resolve(true);
+          }
         }
       });
     } catch (error) {
