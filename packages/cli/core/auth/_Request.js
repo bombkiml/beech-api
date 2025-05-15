a324f57e882c8df2d8b3d200dc3e699242e0922b
bombkiml <nattapat.reh@gmail.com>
2025-05-15 13:38:16 +0700

    :wrench: show catch sql in development mode, fix generate endpoint with sub-folder, minor fixed

--- packages/cli/core/auth/_Request.js
+++ packages/cli/core/auth/_Request.js
@@ -2,7 +2,7 @@ module.exports = {
   requests: (req, res, next) => {
     console.log(res.statusCode);
     console.log(req.url, req.method);
-    return res.status(401).json({
+    return res.status(404).json({
       code: 404,
       status: "404_NOT_FOUND",
       error: "Passport config file not found.",
